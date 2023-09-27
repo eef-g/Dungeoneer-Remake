a45f952b6c0f31c8cb80578c67a3ea8581ff14b0
Ethan Gray <74423016+eef-g@users.noreply.github.com>
2023-09-27 14:25:04 -0700

    Start of backend overhaul

--- backend/lib.go
+++ backend/lib.go
@@ -1,3 +1,29 @@
 package backend
 
+import(
+  "eef.dungeoneer/backend/entities"
+)
 // This is only here so we can access the backend package  & it's subpackages from the frontend
+
+
+// Have structs that will be sent to the frontend as data packets in an event
+type CombatData struct{
+  log string
+  NewRoom RoomData
+} 
+
+type RoomData struct{
+  Enemy entities.Monster
+  RoomNum int
+}
+
+
+func CombatTurn(choice string) CombatData{
+  return CombatData{
+    log: "Test log!",
+    NewRoom: RoomData{
+      Enemy: *entities.CraftMonster("None", 0, 0, 0, "None"),
+      RoomNum: -1,
+    },
+  };
+}
